f3919da6e5b6bf44de18cd33cca5d8818fc3c751
Akgss <akhil.krishnan@valkyrierobotics.com>
2023-04-09 04:13:16 -0700

    stuff

--- Source/js/test.js
+++ Source/js/test.js
@@ -11,7 +11,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-const port = 4000;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
